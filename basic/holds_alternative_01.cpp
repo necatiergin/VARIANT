509653f2dc304eb1ba6864e71f796f0e0859d0bb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-05 14:15:48 +0300

    Update holds_alternative_01.cpp

--- basic/holds_alternative_01.cpp
+++ basic/holds_alternative_01.cpp
@@ -7,13 +7,13 @@ int main()
 	using namespace std;
 
 	cout.setf(std::ios::boolalpha);
-	variant<int, std::string> v = "abc";
+	variant<int, string> v = "abc";
 	cout << "variant holds int     : " << holds_alternative<int>(v) << '\n';
-	cout << "variant holds string  : " << holds_alternative<std::string>(v) << '\n';
+	cout << "variant holds string  : " << holds_alternative<string>(v) << '\n';
 	cout << "\n\n";
 
 	v = 23;
 
 	cout << "variant holds int     : " << holds_alternative<int>(v) << '\n';
-	cout << "variant holds string  : " << holds_alternative<std::string>(v) << '\n';
+	cout << "variant holds string  : " << holds_alternative<string>(v) << '\n';
 }
