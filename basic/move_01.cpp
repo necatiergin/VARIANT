92f1f9e4ca39ddabe2ee97971a3df9c7da831acd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-05 14:17:00 +0300

    Update move_01.cpp

--- basic/move_01.cpp
+++ basic/move_01.cpp
@@ -9,7 +9,7 @@ int main()
 	variant<int, string> v1{ "necati ergin" };
 	cout << "v1.index() = " << v1.index() << '\n';
 	cout << get<1>(v1).size() << '\n';
-	auto v2 = std::move(v1);
+	auto v2 = move(v1);
 	cout << "v1.index() = " << v1.index() << '\n';
 	cout << get<1>(v1).size() << '\n';
 	cout << get<1>(v2).size() << '\n';
