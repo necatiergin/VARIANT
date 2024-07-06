0290e313f389b485e1ddb1d2751ad711a38fbd72
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-06 07:09:55 +0300

    Update visit_01.cpp

--- visit/visit_01.cpp
+++ visit/visit_01.cpp
@@ -1,15 +1,18 @@
 #include <variant>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::variant<char, int, double> var = 'a';
-    
+    std::variant<char, int, double ,std::string> var = 'a';
+
     const auto f{ [](auto x) { std::cout << x << '\n'; } }; //visitor
-    
+
     std::visit(f, var);
     var = 123;
     std::visit(f, var);
     var = 4.7;
+    std::visit(f, var);
+    var = "necati";
     std::visit(f, var);
 }
