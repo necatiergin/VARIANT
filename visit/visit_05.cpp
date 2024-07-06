e71cc429bcc4f3074fcdda060d9f224398625ffc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-06 07:16:51 +0300

    Update visit_05.cpp

--- visit/visit_05.cpp
+++ visit/visit_05.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <string>
 #include <variant>
+#include <string>
 #include <vector>
-#include <type_traits>
+#include <iostream>
 #include <iomanip>
 
 int main()
@@ -10,16 +9,16 @@ int main()
 	using namespace std;
 
 	using vtype = variant<int, double, string>;
-	vector<vtype> vec{ 199, 3.14159, "necati ergin", "hakan koc"};
+	vector<vtype> vec{ 199, 3.14159, "necati ergin", "hakan koc" };
 
 	for (const vtype& val : vec) {
-		std::visit([](const auto& v) {
+		visit([](const auto& v) {
 			if constexpr (std::is_same_v<decltype(v),
-				const std::string&>) {
-				std::cout << quoted(v) << ' ';
+			const std::string&>) {
+				cout << quoted(v) << ' ';
 			}
 			else {
-				std::cout << v << ' ';
+				cout << v << ' ';
 			}
 			},
 			val);
