05ab68979d9b21f2c449ff2cac2802757be543b3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-30 12:15:07 +0300

    Update variant_21.cpp

--- basic/variant_21.cpp
+++ basic/variant_21.cpp
@@ -6,11 +6,9 @@ class Derived : public std::variant<int, std::string> {};
 
 int main()
 {
-	using namespace std;
-
 	Derived d = { "hello" };
-	cout << d.index() << '\n';
-	cout << get<1>(d) << '\n';
+	std::cout << d.index() << '\n';
+	std::cout << get<1>(d) << '\n';
 	d.emplace<0>(77);
-	cout << get<0>(d) << '\n';
+	std::cout << get<0>(d) << '\n';
 }
