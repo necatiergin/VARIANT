8c2d4317bebf9cb190bfbe7693187f8a3a2aff5f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-05 11:43:59 +0300

    Update variant_07.cpp

--- variant_07.cpp
+++ variant_07.cpp
@@ -6,26 +6,26 @@ int main()
 	using namespace std;
 
 	variant<int, double> v1, v2;
-	cout << std::get<int>(v1) << '\n';
+	cout << get<int>(v1) << '\n';
 	v1 = 12; //v1 holds an int
 
-	int& i = std::get<int>(v1);
+	int& i = get<int>(v1);
 	++i;
 	cout << get<int>(v1) << '\n';
 	get<int>(v1) = 20;
-	cout << std::get<int>(v1) << '\n';
+	cout << get<int>(v1) << '\n';
 
-	v2 = std::get<int>(v1);
-	v2 = std::get<0>(v1); // same as above
+	v2 = get<int>(v1);
+	v2 = get<0>(v1); // same as above
 	v2 = v1; // same as above
 
-	//auto f = std::get<float>(v1); // error: variant has no float alternative
-	//auto x = std::get<3>(v1);      // error: invalid index
+	//auto f = get<float>(v1); // error: variant has no float alternative
+	//auto x = get<3>(v1);      // error: invalid index
 
 	try {
 		double dval = get<double>(v2); // current alternative is int not double. throws exception
 	}
 	catch (const bad_variant_access& ex) {
-		std::cout << "exception caught: " << ex.what() << '\n';
+		cout << "exception caught: " << ex.what() << '\n';
 	}
 }
